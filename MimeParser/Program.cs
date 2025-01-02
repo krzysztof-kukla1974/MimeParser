3dfa84b7f745d344d89757f7394c2759e92228d3
krzysztof.kukla <krzysztof.kukla@relativity.com>
2025-01-02 15:42:05 +0100

    Init

---
+++ MimeParser/Program.cs
@@ -0,0 +1,15 @@
+﻿using MimeKit;
+
+string fileName = args[0];
+
+using (var stream = File.OpenRead(fileName))
+{
+    var parser = new MimeParser(stream, MimeFormat.Entity);
+    var message = parser.ParseMessage();
+    Console.WriteLine(message.Subject);
+    Console.WriteLine(message.From.ToString());
+    Console.WriteLine(message.To.ToString());
+    Console.WriteLine(message.Date.ToString());
+    Console.WriteLine(message.TextBody);
+    Console.WriteLine(message.HtmlBody);
+}
